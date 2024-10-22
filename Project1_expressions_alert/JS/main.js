2279b4b8ec38a8725078ed94de11ba4834de46c2
Josh Cordova <cordova.joshuacody@gmail.com>
2024-10-22 12:43:49 -0500

    Basic Javascript Project Submission
    Basic Javascript Project Submission

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+window.alert("This is a window alert") //This is an alert window
+document.write("This is the document.write method.") //This is document.write
+var Sent1 = "This is the beginning of the string" //Var1
+var Sent2 = "and this is the end of the string." //Var2
+document.write(Sent1) + (Sent2) //Concatenate Var1 + Var2
+document.write(14 * 5) //Amount of hours I worked last week
