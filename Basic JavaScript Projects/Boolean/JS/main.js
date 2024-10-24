bdb0f89dbfaba0985dfbdc27d78eefe37513b827
Josh Cordova <cordova.joshuacody@gmail.com>
2024-10-23 20:00:15 -0500

    JS Datatypes

---
+++ Basic JavaScript Projects/Boolean/JS/main.js
@@ -0,0 +1,9 @@
+function not_Function(){
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
+
+document.write(10 > 15 && 15 > 20)
+
+X = "Alicia";
+Y = "Alicia";
+document.write(X==Y)
---
+++ Basic JavaScript Projects/NaN/JSS/main.js
@@ -0,0 +1,7 @@
+function my_Function(){
+    document.getElementById("Test").innerHTML = 0/0;
+}
+
+function my_Function_2(){
+    document.getElementById("True").innerHTML = 10/2;
+}
